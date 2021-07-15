6d42a6f37b9e90672667c234a0c65beddd6a52cc
Nishant Wankhade <82573863+Ngw334@users.noreply.github.com>
2021-07-15 18:12:37 +0530

    Add files via upload

---
+++ UppercaseToLowercase in strings.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string s;
+  cout<< "Give the string : ";
+  cin>> s;
+  cout<< endl;
+  
+  for(int i = 0; i < s.size(); i++)
+  {
+    char ch = s[i];
+    if(int(ch) >= 65 && int(ch) <= 90)
+    {
+      ch += 32;
+      s[i] = ch;
+      continue;
+    }
+    else
+    {
+      continue;
+    } 
+  }
+  cout<< s;
+}
