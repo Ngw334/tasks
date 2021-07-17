efcecde5e861cdfcab4e5377c1a150b110b81440
Nishant Wankhade <82573863+Ngw334@users.noreply.github.com>
2021-07-17 20:33:19 +0530

    Add files via upload

---
+++ 9stringProblem.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector <int> whitespaceIndex(string s)
+{
+  vector <int> index = {0};
+  for(int i = 0; i < s.size()-1; i++)
+  {
+    if(s[i] == ' ')
+    {
+      index.push_back(i-1);
+      index.push_back(i+1);
+      continue;
+    }
+    else{continue;}
+  }
+  index.push_back(s.size()-1);
+  return index;
+}
+
+string reverse(string s, int x, int y)
+{
+    string result = "";
+    for(int i = y; i >= x; i--)
+    {
+        result.push_back(s[i]);
+    }
+    return result;
+}
+
+string answer(string s)
+{
+  string result = "";
+  vector <int> index = whitespaceIndex(s);
+  for(int i = 0; i < index.size(); i += 2)
+  {
+    string x = reverse(s,index[i],index[i+1]);
+    for(int j = 0; j < x.size(); j++)
+    {
+      result.push_back(x[j]);      
+    }
+    result.push_back(' ');
+  }
+  return result;
+}
+
+int main()
+{
+  string s ;
+  cout<< "give the string : ";
+  getline(cin,s);
+  cout<< answer(s);
+  
+  cout<< endl;
+}
